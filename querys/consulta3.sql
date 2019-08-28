90978ddc4480734cf5e24953410ea478fe4d7e36
Carolina Vega <carovegacamejo@gmail.com>
2019-08-27 22:24:37 -0300

    Corregida consulta 3, no habiamos entendido bien la letra xD

--- querys/consulta3.sql
+++ querys/consulta3.sql
@@ -1,11 +1,22 @@
 ﻿--Devolver el nombre de los autores que editaron todas sus 
 --obras en mas de una editorial.
 
-SELECT a.nombre_autor
+SELECT distinct a.nombre_autor
 FROM autores a
 	natural join obra_autor oa
 	natural join obras o
 	natural join obra_editorial oe
 	natural join editoriales e
-group by a.cod_autor
+group by a.cod_autor, oa.cod_obra
 having count(distinct e.cod_editorial) > 1
+/*
+(SELECT count(distinct edi.cod_editorial)
+	FROM autores aut
+		natural join obra_autor oaut
+		natural join obras obr
+		natural join obra_editorial oedi
+		natural join editoriales edi
+	where aut.cod_autor = a.cod_autor
+	group by obr.cod_obra
+	) > 1
+*/
