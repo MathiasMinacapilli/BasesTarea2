17a9b8e6e4cd3e8c1b9d71e26d8734faf3209626
Mathias <mathiasminacapilli@gmail.com>
2019-09-02 21:20:52 -0300

    Updated querys 2 and 4

--- querys/consulta2.sql
+++ querys/consulta2.sql
@@ -1,25 +1,26 @@
 ﻿--Devolver codigo de autor y nombre de los autores que 
 --solamente tuvieron función de autor en todas sus obras.
 
-SELECT a.cod_autor,
+SELECT distinct a.cod_autor,
 	a.nombre_autor
 FROM autores a
 	natural join obra_autor oa
-	natural join funciones f
-where f.cod_funcion = 1
-	and not exists (SELECT a.cod_autor
+where 'autor' in (SELECT distinct func.funcion 
+		FROM autores auts 
+			natural join obra_autor oaut 
+			natural join funciones func 
+		where auts.cod_autor=a.cod_autor)
+	and not exists (SELECT distinct a.cod_autor
 		FROM autores aut
 			natural join obra_autor oaut
-			natural join funciones fun
-		where f.cod_funcion != 1
+		where 'autor' not in (SELECT distinct func.funcion 
+				FROM autores auts 
+					natural join obra_autor oaut 
+					natural join funciones func 
+				where aut.cod_autor=a.cod_autor)
 			and aut.cod_autor = a.cod_autor
 	)
-	--and a.nombre_autor like '%Ercole%'
---group by a.cod_autor
 
---¿Como hacer para chequear cada valor de la columna f.cod_funcion en la bolsa?
---having max(f.cod_funcion) = 1 --cod_funcion 1 es el codigo de la funcion de autor
+--Me fijo que el autor tenga al menos una obra en la que es autor, y luego me fijo que no exista ninguna otra en la que no sea autor. 
 
---El resultado es 1037
-
---Chequear casos en los que puede haber repetidos, ordenar lexicograficamente
+--El resultado es 1037 NOP, atenti al distinct en el cod_autor del select!
