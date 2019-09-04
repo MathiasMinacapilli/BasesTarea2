35efd25bf0020f3894a2c8f3f3588a601a13e8b0
Carolina Vega <carovegacamejo@gmail.com>
2019-09-04 09:11:54 -0300

    garete

--- querys/consulta6.sql
+++ querys/consulta6.sql
@@ -1,1 +1,7 @@
 ﻿--Devolver ćodigo de obra de las obras de Argentina, que tienen un solo autor, y que cumplen que todas lasotras obras de su autor comparten al menos un tema con ella.
+
+SELECT *
+FROM obras o 
+	natural join obra_autor oa
+	natural join funciones f
+where o.pais = 'ARG' and count(f.funcion = 'autor')
---
+++ querys/ejercicio1monitoreo.sql
@@ -0,0 +1,16 @@
+﻿--Devolver título e isbn de las obras que tienen cantidad de editoriales mayor que 2 y cantidad de autores menor que 4
+
+SELECT o.cod_obra, o.titulo, o.isbn 
+FROM obras o
+
+GROUP BY o.cod_obra, o.titulo, o.isbn
+having (SELECT count(oe1.cod_editorial)
+	     FROM obras o1
+		  natural join obra_editorial oe1
+		  where o1.cod_obra = o.cod_obra) > 2
+	and
+
+	(SELECT count(oa1.cod_autor)
+	     FROM obras o2
+		  natural join obra_autor oa1
+		  where o2.cod_obra = o.cod_obra) < 4
