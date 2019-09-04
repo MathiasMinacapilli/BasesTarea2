3309321e272054931a5abb8d2ca1014f06770963
Mathias <mathiasminacapilli@gmail.com>
2019-09-03 22:45:36 -0300

    Cambio nombre consulta 5

--- querys/consulta5.sql
+++ querys/consulta5.sql
@@ -1,12 +1,13 @@
 ﻿--Devolver código y nombre de la editorial con mayor cantidad de obras que son una primera edicion.
 
 --Todas las editoriales (cod_editorial) tal que EDITORIAL(e) and !Existe(EDITORIAL(edi) and count(edi.cant_obras) > edi.cant_obras)
-SELECT e.cod_editorial, e.nombre_editorial as "edicion"
+SELECT e.cod_editorial, 
+	e.nombre_editorial as "edicion"
 FROM editoriales e
 	natural join obras o
 	natural join obra_editorial oe
 where o.edicion like '%1ª%'
-group by e.cod_editorial
+group by e.cod_editorial, e.nombre_editorial
 having not exists (SELECT 1
 	FROM editoriales edi
 		natural join obras ob
