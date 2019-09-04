cd1cb24fc7e075f951fa4ed8850b1347def6fd97
Mathias <mathiasminacapilli@gmail.com>
2019-09-03 23:08:42 -0300

    Cambios consulta 6

--- querys/consulta6.sql
+++ querys/consulta6.sql
@@ -1,11 +1,11 @@
 ﻿--Devolver ćodigo de obra de las obras de Argentina, que tienen un solo autor,
 --y que cumplen que todas las otras obras de su autor comparten al menos un tema con ella.
-SELECT o.cod_obra--o.cod_obra, o.titulo,  o.pais
+SELECT *--o.cod_obra, o.titulo,  o.pais
 FROM obras o
 	natural join obra_autor oa
 where o.pais = 'ARG'
-group by o.cod_obra
-having count(oa.cod_autor) = 1
+/*group by o.cod_obra
+having count(oa.cod_autor) = 1*/
 
 --group by oa.cod_autor
 
